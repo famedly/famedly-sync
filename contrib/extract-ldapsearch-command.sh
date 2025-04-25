f7c533ff76aa15f405d896aa92f74748b37adc8a
Tristan Daniël Maat <t.maat@famedly.com>
2025-04-25 17:07:33 +0800

    feat(contrib): Add a helper script for ldapsearch debugging
    We've needed to figure out what famedly-sync is seeing a few times
    while debugging in customer environments now; this is a quick solution
    that helps get this more easily.
    
    Short of a full command to do various searches in famedly-sync (which
    would basically just reimplement `ldapsearch`) I think this is a
    pretty good way to patch up that use case a bit.

---
+++ contrib/extract-ldapsearch-command.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+config="$1"
+
+if ! [ -e "$config" ]; then
+	echo "Please give the famedly-sync configuration file as the first argument"
+	exit 1
+fi
+
+grep_from_yaml() {
+	grep "$1" "$config" | cut -f 2 -d ':' | tr -d '" '
+}
+
+base_dn="$(grep_from_yaml base_dn)"
+bind_dn="$(grep_from_yaml bind_dn)"
+user_filter="$(grep_from_yaml user_filter)"
+start_tls=""
+if [ "$(grep_from_yaml danger_use_start_tls)" = "true" ]; then
+	start_tls="-Z "
+fi
+if [ "$(grep_from_yaml use_attribute_filter)" = "true" ]; then
+	echo "This command will not include the attribute filter"
+	echo "To reproduce the search exactly, add every attribute name space-separated to the end of the command"
+fi
+
+echo "Please read the password from the config file yourself :)"
+echo "ldapsearch $start_tls-W -b '$base_dn' -D '$bind_dn' '$user_filter'"
